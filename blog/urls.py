b2e8cd4928408f96bee0dd4084e46487c4e47693
Tayler Porter <taylerporter@gmail.com>
2022-05-10 22:27:20 -0500

    In theory got blog working, however there is a syntax error in the template so I cannot confirm.

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    path('', views.PostList.as_view()),
     path('blog/', views.PostList.as_view()),
     path('blog/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
